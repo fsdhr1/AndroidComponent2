d9af98ff34f7b18492a79f59b2a9699876e56a01
ren-Dell <rsf060503@163.com>
2022-11-08 15:59:22 +0800

    自动更新组件 新增检测服务

--- app/src/main/java/com/gykj/commontool/MyApplication.java
+++ app/src/main/java/com/gykj/commontool/MyApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 
 import com.grandtech.mapframe.core.maps.GMapView;
 import com.gykj.grandphotos.config.GrandPhotoHelper;
-//import com.gykj.mvpbasemodule.MvpBaseModule;
+import com.gykj.mvpbasemodule.MvpBaseModule;
 import com.gykj.networkmodule.NetworkHelper;
 import com.mapbox.android.core.crashreporter.CrashReport;
 import com.gykj.autoupdate.utils.UpdateServiceHelper;
@@ -21,7 +21,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        MvpBaseModule.init(this);
+        MvpBaseModule.init(this);
         NetworkHelper.init(this, "http://gykj123.cn:8849/grandtech-service-snyzt/");
         NetworkHelper.setToken(this, "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiLogL_kvbPolYrkuKrkuroiLCJjb250ZXh0VXNlcklkIjoicDlGQjJDQTM4RDREOTQ0MjA4QjAwNDI2MkMwMEM2Rjc4IiwiY29udGV4dE5hbWUiOiJnZW8iLCJjb250ZXh0RGVwdElkIjoiMSIsImNvbnRleHRBcHBsaWNhdGlvbklkIjoiMSIsImV4cCI6MjYxMzIxODk2NDd9.dM92KheaX7Yp7GsHB_iFubyzV8KClL9mFKDy0a1AwXM");
         NetworkHelper.addDefaultSuccessCode(200);
